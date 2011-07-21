fb88e7d92a44399b6677fc1ab9548de5cfbc7874
patrik.lambert <patrik.lambert@8fb776d4-4e0c-0532-85cb-5b6650aa9d8d>
2011-07-21 13:54:28 +0000

    remove absolute path to cmake executable

--- cbuild.sh
+++ cbuild.sh
@@ -2,7 +2,7 @@
 #mkdir -p bin include
 install -d build
 pushd build
-/lium/buster1/servan/Tools/bin/cmake -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 # cmake -DCMAKE_INSTALL_PREFIX=$1  ..
 make -j4 && \
  make install
